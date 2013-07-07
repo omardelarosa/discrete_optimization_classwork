00a64e41f57f57b1bb26984229d710a21493606a
omar delarosa <thedelarosa@gmail.com>
2013-07-07 12:15:03 -0400

    removed global nodes array and used stack instead

--- node_lib.py
+++ node_lib.py
@@ -1,46 +1,56 @@
-all_nodes = []
-
 class Node:
-    def __init__(self, item, room, value, current_best, parent, taken):
+    def __init__(self, data_dict):
         #need to refactor so that it takes an options dictionary called 'data'
-        self.item = item
-        self.room = room
-        self.value = value
-        self.current_best = current_best
-        self.parent = parent
+        #item, room, value, current_best, parent, taken
+        self.data = {}
+        self.data["item"] = data_dict["item"]
+        self.data["room"] = data_dict["room"]
+        self.data["value"] = data_dict["value"]
+        self.data["current_best"] = data_dict["current_best"]
+        self.data["parent"] = data_dict["parent"]
         #is taken?
-        self.taken = taken
+        self.data["taken"] = data_dict["taken"]
         #creates an id for the node that corresponds to its position
         #in the all_nodes array
-        self.id = len(all_nodes)
+        # self.data["id"] = data_dict["id"]
         #adds node to all_nodes
-        all_nodes.append(self)
+        # all_nodes.append(self)
 
-    def get_path(self,acc_array):
-        if self.parent == None:
+    def get_path(self,acc_array,items):
+
+        #make empty array for all item index
+        if acc_array == []:
+            for n in range(0,items):
+                acc_array.append(0)
+
+        if self.data["parent"] == None:
             return acc_array
         else:
-            acc_array.append(self.taken)
-            return self.parent.get_path(acc_array)
+            acc_array[self.data["item"]] = self.data["taken"]
+            
+            # print "At "+str(count)+"..."
+            # print "Acc Array: "
+            # print acc_array
+            return self.data["parent"].get_path(acc_array,items)
             
 
-    def get_siblings(self):
-        return filter(lambda node: True if node.item == self.item and node.parent == self.parent else False, all_nodes)
+    # def get_siblings(self):
+    #     return filter(lambda node: True if node.data["item"] == self.data["item"] and node.data["parent"] == self.data["parent"] else False, all_nodes)
 
-    def get_all_nodes_at_same_level(self):
-        return filter(lambda node: True if node.item == self.item else False, all_nodes)
+    # def get_all_nodes_at_same_level(self):
+    #     return filter(lambda node: True if node.data["item"] == self.data["item"] else False, all_nodes)
 
     def to_s(self):
         print "================="
-        print "My item index is " + str(self.item)
-        print "My id is " + str(self.id)
-        print "My room is " + str(self.room)
-        print "My value is " + str(self.value)
-        print "My parent is " + str(self.parent)
+        print "My item index is " + str(self.data["item"])
+        print "My id is " + str(self.data["id"])
+        print "My room is " + str(self.data["room"])
+        print "My value is " + str(self.data["value"])
+        print "My parent is " + str(self.data["parent"])
         print "================="
 
     def is_root():
-        if self.parent == None:
+        if self.data["parent"] == None:
             return True
         else:
             return False
--- solver.py
+++ solver.py
@@ -6,7 +6,7 @@ from datetime import datetime
 tstart = datetime.now()
 #timer start
 
-execfile("./node_lib.py")
+execfile("node_lib.py")
 
 def solveIt(inputData):
     # Modify this code to run your optimization algorithm
@@ -55,8 +55,8 @@ def solveIt(inputData):
         list_of_item_dicts = make_list_of_item_dicts(values,weights)
         sorted_list_of_item_dicts = sorted(list_of_item_dicts, key=lambda k: k['value_to_weight'])
 
-        print "Sorted items list: "
-        print sorted_list_of_item_dicts
+        # print "Sorted items list: "
+        # print sorted_list_of_item_dicts
 
         #this could be more efficient
         def get_relaxed_estimate():
@@ -88,27 +88,42 @@ def solveIt(inputData):
         current_best = get_relaxed_estimate()
 
         #make root node
-        root_node = Node(-1,capacity,0,current_best,None,False)
+        root_data = {
+            #item, room, value, current_best, parent, taken
+            "item": -1,
+            "room" : capacity,
+            "value" : 0,
+            "current_best" : current_best,
+            "parent" : None,
+            "taken" : False
+            }
+
+        root_node = Node(root_data)
+
+        #keeps track of current best node
+        current_best_node = root_node
+        #keeps a stack of nodes
+        node_stack = []
 
         #debug
-        root_node.to_s()
+        # root_node.to_s()
 
-        #check plausibility of next tree
-        def is_plausible(weight,room):
-            if weight <= room:
-                return True
-            else:
-                return False
+        # #check plausibility of next tree
+        # def is_plausible(weight,room):
+        #     if weight <= room:
+        #         return True
+        #     else:
+        #         return False
 
-        #check plausibility of value
-        def is_continuing_worth_it(current_value,current_best):
-            if current_value >= current_best:
-                return True
-            else:
-                return False
+        # #check plausibility of value
+        # def is_continuing_worth_it(current_value,current_best):
+        #     if current_value >= current_best:
+        #         return True
+        #     else:
+        #         return False
 
         #build tree exhaustively
-        def add_children(item,parent_node):
+        def add_children(item,parent_node,current_best_node):
             #start at 0th item
             # item = 0
 
@@ -117,30 +132,54 @@ def solveIt(inputData):
                 #loop over each item (i.e. level of tree)
                 while item < items:
 
-                    #filter only what plausibly fits
-                    if is_plausible(weights[item],parent_node.room):
+                    value_of_taking_item = parent_node.data["value"]+values[item]
+                    current_room = parent_node.data["room"]
+
+                    #if current item fits and its parent's value is better 
+                    if weights[item] <= current_room:
+                        
                         #make taken node
-                        taken_node = Node(item,parent_node.room-weights[item],parent_node.value+values[item],parent_node.current_best,parent_node,1)
+                        taken_node_data = {
+                        #item, room, value, current_best, parent, taken
+                        "item": item,
+                        "room" : parent_node.data["room"]-weights[item],
+                        "value" : parent_node.data["value"]+values[item],
+                        "current_best" : parent_node.data["current_best"],
+                        "parent" : parent_node,
+                        "taken" : 1
+                        }
+                        
+                        taken_node = Node(taken_node_data)
+
+                        #checks if making node is worth it
+                        if value_of_taking_item > current_best_node.data["value"]:
+
+                            #make current node the new best node
+                            current_best_node = taken_node
+                            #add node to the stack
+                            node_stack.append(taken_node)
                     else:
                         #dont make new node
                         taken_node = False
                     #filter out values 
                     #need to fix
                     #this is not working correctly
-                    if is_continuing_worth_it(parent_node.value,parent_node.current_best):
+                    if parent_node.data["value"] >= parent_node.data["current_best"]:
+                        not_taken_node_data = {
+                            #item, room, value, current_best, parent, taken
+                            "item": item,
+                            "room" : parent_node.data["room"],
+                            "value" : parent_node.data["value"],
+                            "current_best" : parent_node.data["current_best"]-values[item],
+                            "parent" : parent_node,
+                            "taken" : 0
+                            }
                         #make a not-taken node
-                        not_taken_node = Node(item,parent_node.room,parent_node.value,parent_node.current_best-values[item],parent_node,0)
+                        not_taken_node = Node(not_taken_node_data)
                     else:
                         #don't make new node
                         not_taken_node = False
                     
-                    # no filter
-                    # #make taken node
-                    # taken_node = Node(item,parent_node.room-weights[item],parent_node.value+values[item],parent_node.current_best,parent_node,True)
-
-                    # #make a not-taken node
-                    # not_taken_node = Node(item,parent_node.room,parent_node.value,parent_node.current_best-values[item],parent_node,0)
-
                     #debug
                     # taken_node.to_s()
                     # #debug
@@ -148,9 +187,9 @@ def solveIt(inputData):
                     # increment by 1
                     item+=1
                     #stop building tree
-                    add_children(item,not_taken_node)
+                    add_children(item,not_taken_node,current_best_node)
                     #build rest of tree
-                    add_children(item,taken_node)
+                    add_children(item,taken_node,current_best_node)
 
                         
                         #make a tree for item taken
@@ -165,17 +204,17 @@ def solveIt(inputData):
                         #     #if it fits, create a node here
                     
 
-        add_children(0,root_node)
+        add_children(0,root_node,current_best_node)
 
         #debug
         # paths = map(lambda node: node.get_path([]), all_nodes)
         # print paths
 
-        def get_max_value():
+        def get_max_value_old():
             #get all nodes at final level
             max_level_nodes_array = all_nodes[-1].get_all_nodes_at_same_level()
 
-            plausible_maxes = map(lambda node: {'value': node.value, 'id': node.id} if node.room >= 0 else 0, max_level_nodes_array)
+            plausible_maxes = map(lambda node: {'value': node.data["value"], 'id': node.data["id"]} if node.data["room"] >= 0 else 0, max_level_nodes_array)
             # print plausible_maxes
 
             current_max = 0
@@ -186,22 +225,46 @@ def solveIt(inputData):
                     current_max = i["value"]
                     id_of_max = i["id"]
 
-            paths = map(lambda node: node.get_path([]), all_nodes)
+            # paths = map(lambda node: node.get_path([],0), all_nodes)
 
-            path_of_max = all_nodes[id_of_max].get_path([])
+            path_of_max = all_nodes[id_of_max].get_path(taken,items)
 
-            print "Current Max is "+str(current_max)
-            print "Current Max's ID is "+str(id_of_max)
-            print "Path of Max is " +str(path_of_max)
+            # print "Current Max is "+str(current_max)
+            # print "Current Max's ID is "+str(id_of_max)
+            # print "Path of Max is " +str(path_of_max)
             return current_max
 
-        print "Current max list is "+str(get_max_value())
-        print "Current nodes count is "+str(len(all_nodes))
+         
+        def get_max_value():
+            current_max = 0
+            id_of_max = 0
+            max_node = root_node
+
+            plausible_maxes = map(lambda node: {'value': node.data["value"], 'node': node} if node.data["room"] >= 0 else 0, node_stack)
+
+            # print "Plausible Maxes"
+            # print plausible_maxes
+
+            for i in plausible_maxes:
+                if i["value"] > current_max:
+                    current_max = i["value"]
+                    max_node = i["node"]
+
+            path_of_max = max_node.get_path(taken,items)
+            return current_max
+
+        # print "Current max list is "+str(current_max)
+        # print "Current nodes count is "+str(len(all_nodes))
         
 
-        print "Relaxed estimate is: "
-        print get_relaxed_estimate()
+        # print "Relaxed estimate is: "
+        # print get_relaxed_estimate()
+        # print "Node Stack:"
+        # for n in node_stack:
+        #     print n.to_s()
         
+        return get_max_value()
+
         # print "Sorted list of items (by ratio):"
         # print sorted_list
 
@@ -227,8 +290,8 @@ def solveIt(inputData):
     #------------------------
 
     #sets optimal value to result from running algorithm 1
-    print run_alg_3()
-
+    value = run_alg_3()
+    # print value
     #-------------------------
     #--- STOP ALGORITHM 3 ----
     #-------------------------
@@ -254,5 +317,5 @@ if __name__ == '__main__':
         print 'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)'
 
 #timer end
-tend = datetime.now()
-print tend - tstart
+# tend = datetime.now()
+# print tend - tstart
